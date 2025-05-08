1914091672aa4be2afe6608cb8231a4f7653f649
윤혜주 <assaw102@gmail.com>
2025-05-08 16:49:01 +0900

    branch test

--- BestiOSDevelopers/SelfIntroductionData.swift
+++ BestiOSDevelopers/SelfIntroductionData.swift
@@ -42,7 +42,7 @@ let people: [SelfIntroduction] = [
     SelfIntroduction(
         name: "윤혜주",
         trainingPurpose: "iOS 개발자가 되기 위한 훈련 참여",
-        hobby: "음악 감상, 여행, 독서 등",
+        hobby: "음악 감상, 여행",
         mbti: "ENTP",
         strength: "끈기와 실행력",
         sns: nil,
